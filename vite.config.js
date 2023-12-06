deaae3d839c17897ea3ed447bc320365053383b0
Neeraj Pandey <49225459+NEERAJ-PANDEY@users.noreply.github.com>
2023-12-06 21:23:54 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import adapter from '@sveltejs/adapter-vercel';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
